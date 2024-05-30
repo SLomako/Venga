9d490a67585b81e798a14c9565cc9582a0ffac65
Sergei Lomako <lomakosv@gmail.com>
2024-05-30 17:25:48 +0300

    add

--- src/test/java/tests/ManualTest.java
+++ src/test/java/tests/ManualTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import annitations.Manual;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Epic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +12,34 @@ public class ManualTest {
 
     @Manual
     @Test
+    @AllureId("32606")
     @DisplayName("Manual Test Example")
-    void manualTestExample() {
+    @Epic("About Page")
+    void manualTestExampleOne() {
         step("Open MainPage");
-        step("1");
-        step("2");
-        step("3");
+        step("Verify Main Title Text");
+        step("Verify Main Subtitle Text");
+        step("Navigate to About Page");
+    }
+
+    @Manual
+    @Test
+    @DisplayName("Manual Test Example")
+    void manualTestExampleTwo() {
+        step("Open MainPage", () -> {
+            step("Open browser and go to the main URL");
+        });
+
+        step("Verify Main Title Text", () -> {
+            step("Locate the main title element on the page");
+            step("Verify the text of the main title is 'Get in on the future of finance'");
+        });
+
+        step("Verify Main Subtitle Text");
+
+        step("Navigate to About Page", () -> {
+            step("Locate the 'About' link on the main page");
+            step("Click on the 'About' link");
+        });
     }
 }
