8e4e436d75d6f1ff7f7d2f174b7ad6902a2cf3a2
Sergei Lomako <lomakosv@gmail.com>
2024-05-31 14:44:11 +0300

    add

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -62,31 +62,31 @@ public class MainTest extends TestBase {
         });
     }
 
-    @Test
-    @Epic("Download Page")
-    @Feature("Content Verification")
-    @Story("Verify content on the Download page")
-    @Tag("regress")
-    @DisplayName("Navigate to Download page and verify content")
-    @Tag("minor")
-    void navigateToDownloadPageAndVerifyContent() {
-        step("Open Home Page", () -> {
-            homePage
-                    .openPage();
-        });
-        step("Click on the Download link", () -> {
-            homePage
-                    .clickDownloadLink();
-        });
-        step("Verify Download Page header", () -> {
-            downloadPage
-                    .getHeader().shouldHave(text("APP Coming soon"));
-        });
-        step("Verify Download Page description", () -> {
-            downloadPage
-                    .getDescription().shouldHave(text("The Venga app is currently in the final stages of development and will be launched soon. Follow us on our social media channels to stay updated on the app's release."));
-        });
-    }
+//    @Test
+//    @Epic("Download Page")
+//    @Feature("Content Verification")
+//    @Story("Verify content on the Download page")
+//    @Tag("regress")
+//    @DisplayName("Navigate to Download page and verify content")
+//    @Tag("minor")
+//    void navigateToDownloadPageAndVerifyContent() {
+//        step("Open Home Page", () -> {
+//            homePage
+//                    .openPage();
+//        });
+//        step("Click on the Download link", () -> {
+//            homePage
+//                    .clickDownloadLink();
+//        });
+//        step("Verify Download Page header", () -> {
+//            downloadPage
+//                    .getHeader().shouldHave(text("APP Coming soon"));
+//        });
+//        step("Verify Download Page description", () -> {
+//            downloadPage
+//                    .getDescription().shouldHave(text("The Venga app is currently in the final stages of development and will be launched soon. Follow us on our social media channels to stay updated on the app's release."));
+//        });
+//    }
 
 //    @Epic("About Page")
 //    @Feature("Team Member Verification")
